240518e770edca16068ab0768ef4488a74e418fb
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2020-11-18 00:31:45 -0500

    try again for actions

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,17 @@
 push!(LOAD_PATH, "../src/")
 
-using Documenter, Tong
+using Documenter, IDSFiles
 
-makedocs(sitename="IDSFiles.jl Documentation")
+makedocs(
+    sitename="IDSFiles.jl Documentation",
+    format=Documenter.HTML(
+        prettyurls=get(ENV, "CI", nothing) == "true"
+    ),
+    modules=[IDSFiles],
+    pages=[
+        "API Reference" => [
+            "Structures" => "api_structures.md",
+            "Component Dependency Graphs" => "api_depgraphs.md"
+        ]
+    ]
+)
