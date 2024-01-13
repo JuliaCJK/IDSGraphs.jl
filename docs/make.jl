57439789a84df5738227e32dd744e585d3452464
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2024-01-13 13:00:45 -0500

    upgrade to v1 Documenter.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,25 +2,30 @@ push!(LOAD_PATH, "../src/")
 
 using Documenter, IDSGraphs
 
+DocMeta.setdocmeta!(IDSGraphs, :DocTestSetup, :(using IDSGraphs); recursive=true)
+
 makedocs(
     sitename="IDSGraphs.jl Documentation",
     format=Documenter.HTML(
         prettyurls=get(ENV, "CI", nothing) == "true"
     ),
-    modules=[IDSGraphs],
+    #modules=[IDSGraphs],
     pages=[
         "Home" => "index.md",
         "Quick Start" => "guide_quickstart.md",
         "API Reference" => [
             "Structures" => "api_structures.md",
             "Component Dependency Graphs" => "api_depgraphs.md",
-            "Internals" => "api_internals.md"
+            "Internals" => "api_internals.md",
         ]
-    ]
+    ],
+    doctest=false,
 )
 
-deploydocs(
-    repo = "github.com/JuliaCJK/IDSGraphs.jl.git",
-    devbranch = "main",
-    devurl="latest"
+if get(ENV, "CI", nothing) == "true"
+    deploydocs(
+        repo = "github.com/JuliaCJK/IDSGraphs.jl.git",
+        devbranch = "main",
+        devurl="latest",
     )
+end
